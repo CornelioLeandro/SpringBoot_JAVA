0a467a24a5558e55efdfeb1a1f9dfd36ec494c19
Leandro <cornelio.leandro@gmail.com>
2020-08-22 13:25:55 -0300

    Users resources

---
+++ src/main/java/com/cornelioleandro/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.cornelioleandro.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cornelioleandro.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Leandro", "leandr@gmail.com", "14981301912", "123");
+		return ResponseEntity.ok().body(u);
+	}
+}
