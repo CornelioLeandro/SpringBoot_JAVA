efc086fdb867e52dba52ac90bf03f85d7a49f4a9
Leandro <cornelio.leandro@gmail.com>
2020-08-24 20:59:15 -0300

    Product

---
+++ src/main/java/com/cornelioleandro/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.cornelioleandro.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cornelioleandro.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
