ad7020654a9d47a67558681876942fc80a608d71
Leandro <cornelio.leandro@gmail.com>
2020-08-24 20:17:50 -0300

    Order Status

--- src/main/java/com/cornelioleandro/course/config/TestConfig.java
+++ src/main/java/com/cornelioleandro/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.cornelioleandro.course.entities.Order;
 import com.cornelioleandro.course.entities.User;
+import com.cornelioleandro.course.entities.enums.OrderStatus;
 import com.cornelioleandro.course.repositories.OrderRepository;
 import com.cornelioleandro.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1= new User(null, "Leandro Cornelio", "cornelio@gmail.com", "981301912","125874");
 		User u2= new User(null, "Clber Cornelio", "clber@gmail.com", "981301912","125874");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.SHIPPED, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1); 
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
